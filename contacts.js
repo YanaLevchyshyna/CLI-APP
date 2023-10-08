8eaff182ca6cad05e0fba3307876483bccff3559
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-10-08 15:45:02 +0300

    commit

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
+import { nanoid } from 'nanoid';
 
 const contactsPath = path.resolve('db', 'contacts.json');
 // console.log(contactsPath);
@@ -7,4 +8,27 @@ const contactsPath = path.resolve('db', 'contacts.json');
 export const getAllContacts = async () => {
   const contacts = await fs.readFile(contactsPath);
   return JSON.parse(contacts);
+};
+
+export const getContactById = async (id) => {
+  const contacts = await getAllContacts();
+  const oneContact = contacts.find((item) => item.id === id);
+  return oneContact;
+};
+
+export const addContact = async ({ name, email, phone }) => {
+  const contacts = await getAllContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  // метод stringify прибирає всі зайві пробіли, відступи і все записує в один рядок
+  // першим аргументом передаємо те що потрібно перетворити на строку,
+  // другим функція замінник, якщо нам потрібно замінити одні символи на інші, то це передаємо другим аргументом,
+  // третій аргумент - кількість відступів
+  return newContact;
 };
--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 import * as contactsService from './contacts.js';
 
-const contactsAction = async ({ action, id }) => {
+const contactsAction = async ({ action, id, name, phone, email }) => {
   switch (action) {
     case 'list':
       const contactList = await contactsService.getAllContacts();
       return console.log(contactList);
-
+    case 'getById':
+      const oneContact = await contactsService.getContactById(id);
+      return console.log(oneContact);
+    case 'addContact':
+      const newContact = await contactsService.addContact({
+        name,
+        phone,
+        email,
+      });
+      return console.log(newContact);
     default:
       console.log('Unknown action');
       break;
   }
 };
 
-contactsAction({ action: 'list' });
+// contactsAction({ action: 'list' });
+// contactsAction({ action: 'getById', id: 'drsAJ4SHPYqZeG-83QTVW' });
+// contactsAction({ action: 'getById', id: 'qdggE76Jtbfd9eWJHrssH' });
+contactsAction({
+  action: 'addContact',
+  name: 'Anny Bridge',
+  email: 'anny_br@egmail.com',
+  phone: '(098) 120-0056',
+});
+contactsAction({
+  action: 'addContact',
+  name: 'Jacky Simpson',
+  email: 'j_simpson@egmail.com',
+  phone: '(066) 121-4412',
+});
